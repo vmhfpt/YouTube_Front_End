3fd2b9154fe5e52061e07c0496d76b4b6c9a074e
vshuy <vshbmt@gmail.com>
2022-04-03 21:51:02 +0700

    update nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -122,7 +122,7 @@ export function Navbar() {
           class='flex items-center md:order-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
           type='button'
         >
-          Profile{' '}
+          {authState.isLogin ? `Hi ${authState.user.name}` : 'Login'}
           <svg
             class='ml-2 w-4 h-4'
             fill='none'
